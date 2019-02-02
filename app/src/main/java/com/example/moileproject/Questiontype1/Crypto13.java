12dc42d5e6eacf6f965439ad0b96e0adddb0c64d
natza9301 <natlove.natlove202@gmail>
2019-02-02 15:50:04 +0700

    crypto

---
+++ app/src/main/java/com/example/moileproject/Questiontype1/Crypto13.java
@@ -0,0 +1,35 @@
+package com.example.newmobile.Question1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.newmobile.Localstorage;
+import com.example.newmobile.R;
+
+public class Crypto13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto13);
+    }
+    public void btn1(View view){
+        Localstorage.score++;
+        Intent intent = new  Intent(this,Crypto2.class);
+        startActivity(intent);
+    }
+    public void btn2(View view){
+        Intent intent = new  Intent(this,Crypto2.class);
+        startActivity(intent);
+    }
+    public void btn3(View view){
+        Intent intent = new  Intent(this,Crypto2.class);
+        startActivity(intent);
+    }
+    public void btn4(View view){
+        Intent intent = new  Intent(this,Crypto2.class);
+        startActivity(intent);
+    }
+}
